1fa75f830efe021f8e20d0f266c0e71d7d7a1457
digimokan <digimokan@users.noreply.github.com>
2020-06-26 18:11:11 -0500

    Add configure.sh: download roles, run playbook

---
+++ configure.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# use ansible-galaxy cmd to download roles from github/galaxy/etc
+ansible-galaxy install --role-file requirements.yml --roles-path ./roles || exit 1
+
+# run the playbook
+ansible-playbook -i hosts playbook.yml
+
