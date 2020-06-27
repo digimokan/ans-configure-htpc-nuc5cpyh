ee18550970151486ab76b2a690827ea88198847e
digimokan <digimokan@users.noreply.github.com>
2020-06-26 19:36:05 -0500

    ans-galaxy cmd: force-with-deps (always download)

--- configure.sh
+++ configure.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 # use ansible-galaxy cmd to download roles from github/galaxy/etc
-ansible-galaxy role install --role-file requirements.yml --roles-path ./roles || exit 1
+ansible-galaxy role install \
+  --role-file requirements.yml \
+  --roles-path ./roles \
+  --force-with-deps \
+  || exit 1
 
 # run the playbook, passing through args to ansible-playbook cmd
 ansible-playbook -i hosts "${@}" playbook.yml
