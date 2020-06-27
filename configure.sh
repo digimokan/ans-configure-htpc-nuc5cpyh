e32c1267e604534c2191be1c71311f05d2d4b294
digimokan <digimokan@users.noreply.github.com>
2020-06-26 19:11:38 -0500

    configure: add 'role' to ans-galaxy install cmd

--- configure.sh
+++ configure.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # use ansible-galaxy cmd to download roles from github/galaxy/etc
-ansible-galaxy install --role-file requirements.yml --roles-path ./roles || exit 1
+ansible-galaxy role install --role-file requirements.yml --roles-path ./roles || exit 1
 
-# run the playbook
+# run the playbook, passing through args to ansible-playbook cmd
 ansible-playbook -i hosts "${@}" playbook.yml
 
